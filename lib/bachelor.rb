543f0e45fb06d5d40a2b0f0782a0026b06ad8a81
layla dantas <laylatdantas@gmail.com>
2019-06-02 21:36:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,36 @@
+
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+     data.each do |k,v|
+          if k==season
+            v.each do |element|
+              element.each do |k1,v1|
+               if (k1=="name")
+                  return v1.split(" ")[0]
+                end  
+              end
+           end 
+        end 
+    end
 end
-
 def get_contestant_name(data, occupation)
-  # code here
+ data.each do |k,v|
+     v.each do |item|
+       item.each do |k1,v1|
+         if (k1=="occupation")
+           if occupation==v1
+             return item["name"]
+          end
+        end 
+      end
+    end
+  end 
 end
-
 def count_contestants_by_hometown(data, hometown)
-  # code here
+   data.each do |k,v|
+ v.each 
+ 
 end
 
 def get_occupation(data, hometown)
