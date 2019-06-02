e5021b5005cc9688aa2209e15c3beab33094b492
layla dantas <laylatdantas@gmail.com>
2019-06-02 21:49:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,7 +36,7 @@ def get_occupation(data, hometown)
         item.each do |k1,v1|
           if (k1=="hometown")
             if hometown==v1
-              return ["hometown"]
+              return ["name"]
             end 
           end
         end
