2d2088c3447273d50d57bda8f6059e444b1d47bb
layla dantas <laylatdantas@gmail.com>
2019-06-02 21:59:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -28,6 +28,14 @@ def get_contestant_name(data, occupation)
   end 
 end
 def count_contestants_by_hometown(data, hometown)
+ data.each do |k,v|
+   v.each do |item|
+     item.each do |k1,v1|
+       if (k1=="hometown")
+         if hometown==
+ 
+ 
+ 
  
 end
 def get_occupation(data, hometown)
@@ -46,15 +54,7 @@ end
   
 
 def get_average_age_for_season(data, season)
- data.each do |k,v|
-          if k==season
-            
-                end  
-              end
-           end 
-        end 
-    end
- 
+
  
  
  
