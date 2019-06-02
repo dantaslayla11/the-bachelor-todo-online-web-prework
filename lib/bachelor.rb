4c48cf0e60ae33f0e271b466a75dde5122db9138
layla dantas <laylatdantas@gmail.com>
2019-06-02 22:22:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -70,9 +70,7 @@ def get_average_age_for_season(data, season)
       end
     end  
   end
-  puts total
-  puts number_of_contestants
-  puts (total/number_of_contestants.to_f).round
-  #(total/number_of_contestants.to_f).ceil
+  (total/number_of_contestants.to_f).round
+  
   
 end
